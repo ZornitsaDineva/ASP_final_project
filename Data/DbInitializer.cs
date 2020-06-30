902a63a4d2a9c324bd608068cbfd4d3e3334c516
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2020-06-30 11:16:55 +0300

    fix data base

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -15,27 +15,27 @@ namespace ContosoUniversity.Data
             // Look for any students.
             if (context.Students.Any())
             {
-                return;   // DB has been seeded
+                 return;   // DB has been seeded
             }
 
             var students = new Student[]
             {
-                new Student { FirstMidName = "Carson",   LastName = "Alexander",
+                new Student { FirstMidName = "Мила",   LastName = "Милева",
                     EnrollmentDate = DateTime.Parse("2010-09-01") },
-                new Student { FirstMidName = "Meredith", LastName = "Alonso",
+                new Student { FirstMidName = "Мария", LastName = "Петрова",
                     EnrollmentDate = DateTime.Parse("2012-09-01") },
-                new Student { FirstMidName = "Arturo",   LastName = "Anand",
+                new Student { FirstMidName = "Пенка",   LastName = "Димова",
                     EnrollmentDate = DateTime.Parse("2013-09-01") },
-                new Student { FirstMidName = "Gytis",    LastName = "Barzdukas",
+                new Student { FirstMidName = "Димитър",    LastName = "Паскалев",
                     EnrollmentDate = DateTime.Parse("2012-09-01") },
-                new Student { FirstMidName = "Yan",      LastName = "Li",
+                new Student { FirstMidName = "Жан",      LastName = "Валжан",
                     EnrollmentDate = DateTime.Parse("2012-09-01") },
-                new Student { FirstMidName = "Peggy",    LastName = "Justice",
+                new Student { FirstMidName = "Ния",    LastName = "Желязкова",
                     EnrollmentDate = DateTime.Parse("2011-09-01") },
-                new Student { FirstMidName = "Laura",    LastName = "Norman",
+                new Student { FirstMidName = "Лаура",    LastName = "Ботева",
                     EnrollmentDate = DateTime.Parse("2013-09-01") },
-                new Student { FirstMidName = "Nino",     LastName = "Olivetto",
-                    EnrollmentDate = DateTime.Parse("2005-09-01") }
+                new Student { FirstMidName = "Нино",     LastName = "Пиронков",
+                    EnrollmentDate = DateTime.Parse("2025-09-01") }
             };
 
             foreach (Student s in students)
@@ -46,15 +46,15 @@ namespace ContosoUniversity.Data
 
             var instructors = new Instructor[]
             {
-                new Instructor { FirstMidName = "Kim",     LastName = "Abercrombie",
+                new Instructor { FirstMidName = "Александър",     LastName = "Добрев",
                     HireDate = DateTime.Parse("1995-03-11") },
-                new Instructor { FirstMidName = "Fadi",    LastName = "Fakhouri",
+                new Instructor { FirstMidName = "Симеон",    LastName = "Иванов",
                     HireDate = DateTime.Parse("2002-07-06") },
-                new Instructor { FirstMidName = "Roger",   LastName = "Harui",
+                new Instructor { FirstMidName = "Крум",   LastName = "Славов",
                     HireDate = DateTime.Parse("1998-07-01") },
-                new Instructor { FirstMidName = "Candace", LastName = "Kapoor",
+                new Instructor { FirstMidName = "Ния", LastName = "Златанова",
                     HireDate = DateTime.Parse("2001-01-15") },
-                new Instructor { FirstMidName = "Roger",   LastName = "Zheng",
+                new Instructor { FirstMidName = "Петя",   LastName = "Вълканова",
                     HireDate = DateTime.Parse("2004-02-12") }
             };
 
@@ -66,18 +66,18 @@ namespace ContosoUniversity.Data
 
             var departments = new Department[]
             {
-                new Department { Name = "English",     Budget = 350000,
+                new Department { Name = "Макро икономика",     Budget = 350000,
                     StartDate = DateTime.Parse("2007-09-01"),
-                    InstructorID  = instructors.Single( i => i.LastName == "Abercrombie").ID },
-                new Department { Name = "Mathematics", Budget = 100000,
+                    InstructorID  = instructors.Single( i => i.LastName == "Славов").ID },
+                new Department { Name = "Микро икономика", Budget = 100000,
                     StartDate = DateTime.Parse("2007-09-01"),
-                    InstructorID  = instructors.Single( i => i.LastName == "Fakhouri").ID },
-                new Department { Name = "Engineering", Budget = 350000,
+                    InstructorID  = instructors.Single( i => i.LastName == "Иванов").ID },
+                new Department { Name = "Информатика", Budget = 350000,
                     StartDate = DateTime.Parse("2007-09-01"),
-                    InstructorID  = instructors.Single( i => i.LastName == "Harui").ID },
-                new Department { Name = "Economics",   Budget = 100000,
+                    InstructorID  = instructors.Single( i => i.LastName == "Добрев").ID },
+                new Department { Name = "Екология",   Budget = 100000,
                     StartDate = DateTime.Parse("2007-09-01"),
-                    InstructorID  = instructors.Single( i => i.LastName == "Kapoor").ID }
+                    InstructorID  = instructors.Single( i => i.LastName == "Златанова").ID }
             };
 
             foreach (Department d in departments)
@@ -88,26 +88,26 @@ namespace ContosoUniversity.Data
 
             var courses = new Course[]
             {
-                new Course {CourseID = 1050, Title = "Chemistry",      Credits = 3,
-                    DepartmentID = departments.Single( s => s.Name == "Engineering").DepartmentID
+                new Course {CourseID = 1050, Title = "Фондови пазари",      Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "Макро икономика").DepartmentID
                 },
-                new Course {CourseID = 4022, Title = "Microeconomics", Credits = 3,
-                    DepartmentID = departments.Single( s => s.Name == "Economics").DepartmentID
+                new Course {CourseID = 4022, Title = "Производителност на труда", Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "Микро икономика").DepartmentID
                 },
-                new Course {CourseID = 4041, Title = "Macroeconomics", Credits = 3,
-                    DepartmentID = departments.Single( s => s.Name == "Economics").DepartmentID
+                new Course {CourseID = 4041, Title = "Програмиране", Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "Информатика").DepartmentID
                 },
-                new Course {CourseID = 1045, Title = "Calculus",       Credits = 4,
-                    DepartmentID = departments.Single( s => s.Name == "Mathematics").DepartmentID
+                new Course {CourseID = 1045, Title = "Връзки с обществеността",       Credits = 4,
+                    DepartmentID = departments.Single( s => s.Name == "Микро икономика").DepartmentID
                 },
-                new Course {CourseID = 3141, Title = "Trigonometry",   Credits = 4,
-                    DepartmentID = departments.Single( s => s.Name == "Mathematics").DepartmentID
+                new Course {CourseID = 3141, Title = "Работа в екип",   Credits = 4,
+                    DepartmentID = departments.Single( s => s.Name == "Информатика").DepartmentID
                 },
-                new Course {CourseID = 2021, Title = "Composition",    Credits = 3,
-                    DepartmentID = departments.Single( s => s.Name == "English").DepartmentID
+                new Course {CourseID = 2021, Title = "Опазване на околната среда",    Credits = 3,
+                    DepartmentID = departments.Single( s => s.Name == "Екология").DepartmentID
                 },
-                new Course {CourseID = 2042, Title = "Literature",     Credits = 4,
-                    DepartmentID = departments.Single( s => s.Name == "English").DepartmentID
+                new Course {CourseID = 2042, Title = "Емоционална интелегентност",     Credits = 4,
+                    DepartmentID = departments.Single( s => s.Name == "Информатика").DepartmentID
                 },
             };
 
@@ -120,56 +120,53 @@ namespace ContosoUniversity.Data
             var officeAssignments = new OfficeAssignment[]
             {
                 new OfficeAssignment {
-                    InstructorID = instructors.Single( i => i.LastName == "Fakhouri").ID,
-                    Location = "Smith 17" },
+                    InstructorID = instructors.Single( i => i.LastName == "Златанова").ID,
+                    Location = "Стара Загора" },
                 new OfficeAssignment {
-                    InstructorID = instructors.Single( i => i.LastName == "Harui").ID,
-                    Location = "Gowan 27" },
+                    InstructorID = instructors.Single( i => i.LastName == "Иванов").ID,
+                    Location = "Нова Згора" },
                 new OfficeAssignment {
-                    InstructorID = instructors.Single( i => i.LastName == "Kapoor").ID,
-                    Location = "Thompson 304" },
+                    InstructorID = instructors.Single( i => i.LastName == "Добрев").ID,
+                    Location = "София" },
             };
 
             foreach (OfficeAssignment o in officeAssignments)
             {
                 context.OfficeAssignments.Add(o);
             }
+            
             context.SaveChanges();
 
             var courseInstructors = new CourseAssignment[]
             {
                 new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Chemistry" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Kapoor").ID
+                    CourseID = courses.Single(c => c.Title == "Фондови пазари" ).CourseID,
+                    InstructorID = instructors.Single(i => i.LastName == "Златанова").ID
                     },
                 new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Chemistry" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Harui").ID
+                    CourseID = courses.Single(c => c.Title == "Производителност на труда" ).CourseID,
+                    InstructorID = instructors.Single(i => i.LastName == "Иванов").ID
                     },
                 new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Microeconomics" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Zheng").ID
+                    CourseID = courses.Single(c => c.Title == "Програмиране" ).CourseID,
+                    InstructorID = instructors.Single(i => i.LastName == "Добрев").ID
                     },
                 new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Macroeconomics" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Zheng").ID
+                    CourseID = courses.Single(c => c.Title == "Опазване на околната среда" ).CourseID,
+                    InstructorID = instructors.Single(i => i.LastName == "Вълканова").ID
                     },
                 new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Calculus" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Fakhouri").ID
+                    CourseID = courses.Single(c => c.Title == "Връзки с обществеността" ).CourseID,
+                    InstructorID = instructors.Single(i => i.LastName == "Славов").ID
                     },
                 new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Trigonometry" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Harui").ID
+                    CourseID = courses.Single(c => c.Title == "Работа в екип" ).CourseID,
+                    InstructorID = instructors.Single(i => i.LastName == "Вълканова").ID
                     },
                 new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Composition" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Abercrombie").ID
-                    },
-                new CourseAssignment {
-                    CourseID = courses.Single(c => c.Title == "Literature" ).CourseID,
-                    InstructorID = instructors.Single(i => i.LastName == "Abercrombie").ID
-                    },
+                    CourseID = courses.Single(c => c.Title == "Емоционална интелегентност" ).CourseID,
+                    InstructorID = instructors.Single(i => i.LastName == "Славов").ID
+                    }
             };
 
             foreach (CourseAssignment ci in courseInstructors)
@@ -181,57 +178,57 @@ namespace ContosoUniversity.Data
             var enrollments = new Enrollment[]
             {
                 new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Alexander").ID,
-                    CourseID = courses.Single(c => c.Title == "Chemistry" ).CourseID,
+                    StudentID = students.Single(s => s.LastName == "Димова").ID,
+                    CourseID = courses.Single(c => c.Title == "Фондови пазари" ).CourseID,
                     Grade = Grade.A
                 },
                     new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Alexander").ID,
-                    CourseID = courses.Single(c => c.Title == "Microeconomics" ).CourseID,
+                    StudentID = students.Single(s => s.LastName == "Милева").ID,
+                    CourseID = courses.Single(c => c.Title == "Производителност на труда" ).CourseID,
                     Grade = Grade.C
                     },
                     new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Alexander").ID,
-                    CourseID = courses.Single(c => c.Title == "Macroeconomics" ).CourseID,
+                    StudentID = students.Single(s => s.LastName == "Валжан").ID,
+                    CourseID = courses.Single(c => c.Title == "Работа в екип" ).CourseID,
                     Grade = Grade.B
                     },
                     new Enrollment {
-                        StudentID = students.Single(s => s.LastName == "Alonso").ID,
-                    CourseID = courses.Single(c => c.Title == "Calculus" ).CourseID,
+                        StudentID = students.Single(s => s.LastName == "Петрова").ID,
+                    CourseID = courses.Single(c => c.Title == "Програмиране" ).CourseID,
                     Grade = Grade.B
                     },
                     new Enrollment {
-                        StudentID = students.Single(s => s.LastName == "Alonso").ID,
-                    CourseID = courses.Single(c => c.Title == "Trigonometry" ).CourseID,
+                        StudentID = students.Single(s => s.LastName == "Паскалев").ID,
+                    CourseID = courses.Single(c => c.Title == "Опазване на околната среда" ).CourseID,
                     Grade = Grade.B
                     },
                     new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Alonso").ID,
-                    CourseID = courses.Single(c => c.Title == "Composition" ).CourseID,
+                    StudentID = students.Single(s => s.LastName == "Пиронков").ID,
+                    CourseID = courses.Single(c => c.Title == "Връзки с обществеността" ).CourseID,
                     Grade = Grade.B
                     },
                     new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Anand").ID,
-                    CourseID = courses.Single(c => c.Title == "Chemistry" ).CourseID
+                    StudentID = students.Single(s => s.LastName == "Петрова").ID,
+                    CourseID = courses.Single(c => c.Title == "Фондови пазари" ).CourseID
                     },
                     new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Anand").ID,
-                    CourseID = courses.Single(c => c.Title == "Microeconomics").CourseID,
+                    StudentID = students.Single(s => s.LastName == "Ботева").ID,
+                    CourseID = courses.Single(c => c.Title == "Фондови пазари").CourseID,
                     Grade = Grade.B
                     },
                 new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Barzdukas").ID,
-                    CourseID = courses.Single(c => c.Title == "Chemistry").CourseID,
+                    StudentID = students.Single(s => s.LastName == "Димова").ID,
+                    CourseID = courses.Single(c => c.Title == "Програмиране").CourseID,
                     Grade = Grade.B
                     },
                     new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Li").ID,
-                    CourseID = courses.Single(c => c.Title == "Composition").CourseID,
+                    StudentID = students.Single(s => s.LastName == "Петрова").ID,
+                    CourseID = courses.Single(c => c.Title == "Връзки с обществеността").CourseID,
                     Grade = Grade.B
                     },
                     new Enrollment {
-                    StudentID = students.Single(s => s.LastName == "Justice").ID,
-                    CourseID = courses.Single(c => c.Title == "Literature").CourseID,
+                    StudentID = students.Single(s => s.LastName == "Димова").ID,
+                    CourseID = courses.Single(c => c.Title == "Работа в екип").CourseID,
                     Grade = Grade.B
                     }
             };
