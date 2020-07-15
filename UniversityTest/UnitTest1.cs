1175e19eb682f09490228481504c8c7b73a95984
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2020-07-15 17:00:31 +0300

    add test conditions

--- UniversityTest/UnitTest1.cs
+++ UniversityTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using ContosoUniversity.Controllers;
 using ContosoUniversity.Data;
+using ContosoUniversity.Models;
 using ContosoUniversity.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -36,6 +37,16 @@ namespace TestUniversity
 
             Assert.That(instructors.Cast<object>().Count(), Is.EqualTo(5));
             Assert.That(actResult.ViewName, Is.EqualTo(null));
+
+            IEnumerator enumerator = instructors.GetEnumerator();
+            enumerator.MoveNext();
+            Instructor instructor = (Instructor)enumerator.Current;
+            CourseAssignment ca = instructor.CourseAssignments.First();
+            Course course = ca.Course;
+            Department department = course.Department;
+
+            Assert.That(course, Is.Not.Null);
+            Assert.That(department, Is.Not.Null);
         }
 
         [Test]
