c6c3f97b47f70fe2833a33d6fcf9e83c36a8b413
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2020-08-31 13:11:08 +0300

    add external Microsoft login

--- University/Areas/Identity/IdentityHostingStartup.cs
+++ University/Areas/Identity/IdentityHostingStartup.cs
@@ -8,21 +8,41 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
+
 [assembly: HostingStartup(typeof(ContosoUniversity.Areas.Identity.IdentityHostingStartup))]
 namespace ContosoUniversity.Areas.Identity
 {
     public class IdentityHostingStartup : IHostingStartup
     {
+
+
         public void Configure(IWebHostBuilder builder)
         {
-            builder.ConfigureServices((context, services) => {
+            builder.ConfigureServices((context, services) =>
+            {
                 services.AddDbContext<ContosoUniversityContext>(options =>
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("ContosoUniversityContextConnection")));
 
                 services.AddDefaultIdentity<ContosoUniversityUser>()
                     .AddEntityFrameworkStores<ContosoUniversityContext>();
+
+                services.AddAuthentication()
+                    .AddMicrosoftAccount(microsoftOptions =>
+                    {
+                        microsoftOptions.ClientId = context.Configuration["Authentication:Microsoft:ClientId"];
+                        microsoftOptions.ClientSecret = context.Configuration["Authentication:Microsoft:ClientSecret"];
+                    });
+                    /*.AddGoogle(googleOptions => { })
+                    .AddTwitter(twitterOptions => { })
+                    .AddFacebook(facebookOptions =>
+                    {
+                        facebookOptions.AppId = context.Configuration["Authentication:facebook:appid"];
+                        facebookOptions.AppSecret = context.Configuration["Authentication:facebook:appsecret"];
+                    });*/
             });
         }
+       
     }
 }
